a76493705d272680c3f508fc9306df6cd07be05a
Amra Halaba <amrahalaba@outlook.com>
2023-02-01 11:09:41 +0100

    Create and Edit Forms.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express  = require('express');
 const mongoose = require('mongoose');
 const path     = require('path');
-const flash    = require('flash');
+const flash    = require('connect-flash');
 const session  = require('express-session')
 //const morgan   = require('morgan');
 
--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,7 +1,7 @@
 
 //-------------------------------------------------------------------------------------------------------------------------------------
 //IMPORTS
-const Post = require('../models/PostModel').Post;
+const Post = require('../models/PostModel');
 
 //-------------------------------------------------------------------------------------------------------------------------------------
 //EXPORTS
@@ -12,26 +12,42 @@ module.exports = {
     },
 
     getPosts: (req, res) => {
-        res.send('All Posts');
+        Post.find()
+            .then(posts => {
+                res.render("admin/posts/index", { title: "All Posts", posts: posts });
+            });
     },    
 
     submitPosts: (req, res) => {
+
+        //form data validation pending
+
+
         const newPost = new Post({
-            title: req.body.title,
-            description: req.body.description,
-            status: req.body.status,
+          title: req.body.title,
+          description: req.body.description,
+          status: req.body.status,
+          allowComments: req.body.allowComments,
         });
 
         newPost.save()
             .then(post => {
                 console.log(post);
                 req.flash('success-message', "Post created successfully.")
-                res.redirect('/admin/posts');
+                res.redirect('/admin');
             });
     },
 
     createPosts: (req, res) => {
         res.render('admin/posts/create', {title: 'Create Post'});
+    },
+
+    editPosts: (req, res) => {
+        const id = req.params.id;
+        Post.findById(id)
+            .then(post => {
+                res.render("admin/posts/edit", { title: "Edit Post", post: post });
+            });
     },
 
 };
